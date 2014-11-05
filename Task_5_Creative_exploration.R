70126e25e8b3dc37ca3dcf453952d6fc2aad6947
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-05 17:17:13 +1100

    start

---
+++ Task_5_Creative_exploration.R
@@ -0,0 +1,18 @@
+###########################
+### Tasks to accomplish ###
+###########################
+# 1. Explore new models and data to improve your predictive model.
+# 2. Evaluate your new predictions on both accuracy and efficiency. 
+
+#############################
+### Questions to consider ###
+#############################
+# 1. What are some alternative data sets you could consider using? 
+# 2. What are ways in which the n-gram model may be inefficient?
+# 3. What are the most commonly missed n-grams? Can you think of a reason why they would be missed and fix that? 
+# 4. What are some other things that other people have tried to improve their model? 
+# 5. Can you estimate how uncertain you are about the words you are predicting? 
+
+##############
+### Script ###
+##############
---
+++ Task_6_Data_product.R
@@ -0,0 +1,24 @@
+###########################
+### Tasks to accomplish ###
+###########################
+# 1. Create a data product to show off your prediction algorithm You should create a Shiny app that 
+# accepts an n-gram and predicts the next word.
+
+#############################
+### Questions to consider ###
+#############################
+# 1. What are the most interesting ways you could show off your algorithm?
+# 2. Are there any data visualizations you think might be helpful 
+# (look at the Swiftkey data dashboard if you have it loaded on your phone)?
+# 3. How should you document the use of your data product (separately from how you created it) 
+# so that others can rapidly deploy your algorithm?
+
+###############################
+### Tips, tricks, and hints ###
+###############################
+# 1. Consider the size of the predictive model you have developed. 
+# You may have to sacrifice some accuracy to have a fast enough/small enough model to load into Shiny. 
+ 
+##############
+### Script ###
+##############
