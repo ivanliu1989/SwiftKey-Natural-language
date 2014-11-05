1966d68ae499161f0bcab9bd646be0e5cbf825a8
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-05 17:18:55 +1100

    start

---
+++ Task_7_Slide_deck.R
@@ -0,0 +1,16 @@
+###########################
+### Tasks to accomplish ###
+###########################
+# 1. Create a slide deck promoting your product. 
+# Write 5 slides using RStudio Presenter explaining your product and why it is awesome!
+
+#############################
+### Questions to consider ###
+#############################
+# 1. How can you briefly explain how your predictive model works?
+# 2. How can you succinctly quantitatively summarize the performance of your prediction algorithm?
+# 3. How can you show the user how the product works?
+
+##############
+### Script ###
+##############
