42ab74914b07a44669adf8332a23b153b161e8c6
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-07 13:16:04 +1100

    tokenization func

--- Task_1.5_Tokenization_func.R
+++ Task_1.5_Tokenization_func.R
@@ -1,85 +1,85 @@
 tokenization <- function (docs, trans = c(T,T,T,T,T,T,T,T), ChartoSpace = c('/','@','\\|'),
               stopWords = 'english', ownStopWords = c(), profanity = data.frame()) {
     
-    print(paste('Please wait for initializing and summrising the input files......'))
+    cat(paste('/nPlease wait for initializing and summrising the input files......'))
     require(tm); require(SnowballC)
-    print(paste('There are', 'nchar(docs)', 'characters in input/unprocessed document!'))
-    print(summary(docs))
-    print(paste('Start tokenization processes...'))
+    cat(paste('/nThere are', 'nchar(docs)', 'characters in input/unprocessed document!'))
+    cat(summary(docs))
+    cat(paste('/nStart tokenization processes...'))
     
     # Simple Transformation
-    print(paste('Simple Transformation:', trans[1]))
+    cat(paste('/nSimple Transformation:', trans[1]))
     if(trans[1] == T){
         toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
         for (i in ChartoSpace){
             docs <- tm_map(docs, toSpace, i)
-            print(paste('Character:', i, 'has been transformed into white space!'))
+            cat(paste('->Character:', i, 'has been transformed into white space!'))
         }
     }
     
     # Lowercase
-    print(paste('Lowercase Transformation:', trans[2]))
+    cat(paste('/nLowercase Transformation:', trans[2]))
     if(trans[2] == T){
         docs <- tm_map(docs, content_transformer(tolower))
-        print('All CAPITAL characters have been transformed to lower cases!')
+        cat('->All CAPITAL characters have been transformed to lower cases!')
     }
     
     # Remove Numbers
-    print(paste('Remove Numbers:', trans[3]))
+    cat(paste('/nRemove Numbers:', trans[3]))
     if(trans[3] == T){
         docs <- tm_map(docs, removeNumbers)
-        print('All NUMBERs have been eliminated from raw document!')
+        cat('->All NUMBERs have been eliminated from raw document!')
     }
     
     # Remove Punctuations
-    print(paste('Remove Punctuations:', trans[4]))
+    cat(paste('/nRemove Punctuations:', trans[4]))
     if(trans[4] == T){
         docs <- tm_map(docs, removePunctuation)
-        print('All Punctuations have been eliminated from raw document!')
+        cat('->All Punctuations have been eliminated from raw document!')
     }
     
     # Remove English Stop Words
-    print(paste('Remove Stop Words:', trans[5]))
+    cat(paste('/nRemove Stop Words:', trans[5]))
     if(trans[5] == T){
-        print(paste('Remove', stopWords, 'Stop Words:'))
-        print(paste('Stop Words including:' ))
-        print(stopwords(stopWords))
-        print(paste(length(stopwords(stopWords)), 'words in total'))
+        cat(paste('->Remove', stopWords, 'Stop Words:'))
+        cat(paste('->Stop Words including:' ))
+        cat(stopwords(stopWords))
+        cat(paste('->',length(stopwords(stopWords)), 'words in total'))
         docs <- tm_map(docs, removeWords, stopwords(stopWords))
-        print('Stop Words have been eliminated from raw document!')
+        cat('->Stop Words have been eliminated from raw document!')
     }
     
     # Remove Own Stop Words
-    print(paste('Remove Own Stop Words:', trans[6]))
+    cat(paste('/nRemove Own Stop Words:', trans[6]))
     if(trans[6] == T){
-        print(paste('Remove Own Stop Words:'))
-        print(paste('Stop Words including:' ))
-        print(ownStopWords)
-        print(paste(length(ownStopWords), 'words in total'))
+        cat(paste('->Remove Own Stop Words:'))
+        cat(paste('->Stop Words including:' ))
+        cat(ownStopWords)
+        cat('->', paste(length(ownStopWords), 'words in total'))
         docs <- tm_map(docs, removeWords, ownStopWords)
-        print('Own Stop Words have been eliminated from raw document!')
+        cat('->Own Stop Words have been eliminated from raw document!')
     }
     
     # Strip Whitespace
-    print(paste('Strip Whitespace:', trans[7]))
+    cat(paste('/nStrip Whitespace:', trans[7]))
     if(trans[7] == T){
         docs <- tm_map(docs, stripWhitespace)
-        print('Whitespaces have been stripped from raw document!')
+        cat('->Whitespaces have been stripped from raw document!')
     }
     
     # Specific Transformations/Profanity filtering
-    print(paste('Specific Transformations/Profanity filtering:', trans[8]))
+    cat(paste('/nSpecific Transformations/Profanity filtering:', trans[8]))
     if(trans[8] == T){
         toString <- content_transformer(function(x, from, to) gsub(from, to, x))
         for (i in nrow(profanity)){
-            print(paste('Transfer', profanity[i,1], 'to', profanity[i,2]))
+            cat(paste('->Transfer', profanity[i,1], 'to', profanity[i,2]))
             docs <- tm_map(docs, toString, profanity[i,1], profanity[i,2])
         }
-        print('Specific Transformations/Profanity filtering have been done to raw document!')
+        cat('->Specific Transformations/Profanity filtering have been done to raw document!')
     }
     
     # Complete messages
-    print('Document has been tokenized!')
-    print(summary(docs))
+    cat('/nDocument has been tokenized!')
+    cat(summary(docs))
     return(docs)
 }
