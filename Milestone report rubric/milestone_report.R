1bfcc9ffaa2363a2db9af29a4946f59b850465df
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-12 22:03:18 +1100

    update

--- Milestone report rubric/milestone_report.R
+++ Milestone report rubric/milestone_report.R
@@ -11,6 +11,7 @@ require(tm); require(SnowballC); require(data.table)
 require(ggplot2); require(RWeka); require(qdap);
 require(scales); require(gridExtra)
 
+## 1 ##
 stem_path <- file.path('.','final/en_US')
 dir(stem_path)
 
@@ -33,7 +34,31 @@ lengths$names <- c("blogs","news","twitter")
 ggplot(lengths,aes(x=names,y=lengths)) +
     geom_bar(stat='identity',fill='cornsilk',color='grey60') + 
     xlab('Source') + ylab('Total Lines') + coord_flip() + 
-    geom_text(aes(label=format(lengths,big.mark=","),size=3),labels=',',vjust=-0.2) +
+    geom_text(aes(label=format(lengths,big.mark=","),size=3),labels=comma,vjust=-0.2) +
     scale_y_continuous(limits=c(0,2600000),labels=comma) +
     theme(legend.position='none') + 
     ggtitle('Total Line Count by Text Source')
+
+## 2 ##
+en_US_blogs <- gsub("[^[:alpha:][:space:]']", " ", en_US_blogs)
+en_US_blogs <- gsub("â ", "'", en_US_blogs)
+en_US_blogs <- gsub("ã", "'", en_US_blogs)
+en_US_blogs <- gsub("ð", "'", en_US_blogs)
+
+en_US_news <- gsub("[^[:alpha:][:space:]']", " ", en_US_news)
+en_US_news <- gsub("â ", "'", en_US_news)
+en_US_news <- gsub("ã", "'", en_US_news)
+en_US_news <- gsub("ð", "'", en_US_news)
+
+en_US_twitter<- gsub("[^[:alpha:][:space:]']", " ", en_US_twitter)
+en_US_twitter <- gsub("â ", "'", en_US_twitter)
+en_US_twitter <- gsub("ã", "'", en_US_twitter)
+en_US_twitter <- gsub("ð", "'", en_US_twitter)
+
+en_US_blogs <- Trim(clean(en_US_blogs))
+en_US_news <- Trim(clean(en_US_news))
+en_US_twitter <- Trim(clean(en_US_twitter))
+
+en_US_blogs <- tolower(en_US_blogs)
+en_US_news <- tolower(en_US_news)
+en_US_twitter <- tolower(en_US_twitter)
