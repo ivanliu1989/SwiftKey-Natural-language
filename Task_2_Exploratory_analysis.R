c455b3f152c3f677e7e32026114aee5005ede988
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-12 12:04:17 +1100

    save txt

--- Task_2_Exploratory_analysis.R
+++ Task_2_Exploratory_analysis.R
@@ -21,3 +21,48 @@
 ##############
 ### Script ###
 ##############
+setwd('H:/Machine Learning/SwiftKey/')
+setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\SwiftKey')
+setwd('/Users/ivan/Work_directory/SwiftKey')
+rm(list=ls(all=TRUE));gc(reset=TRUE);par(mfrow=c(1,1))
+require(tm); require(SnowballC); require(data.table); library("RWeka")
+
+# ngrams_test <- NGramTokenizer(stem_docs, Weka_control(min = 3, max = 3, delimiters = " \\r\\n\\t.,;:\"()?!"))
+BigramTokenizer <- function(x) 
+    NGramTokenizer(x, Weka_control(min = 2, max = 2))
+TrigramTokenizer <- function(x) 
+    NGramTokenizer(x, Weka_control(min = 3, max = 3))
+
+############################
+### Document Term Matrix ###
+############################
+load('data/en_US.blogs.txt_stem.RData') # stem_docs
+class(stem_docs)
+
+dtm_docs <- DocumentTermMatrix(stem_docs, control = list(tokenize = TrigramTokenizer))
+dtm_docs <- DocumentTermMatrix(stem_docs) # No ngrams
+# tdm_docs <- TermDocumentMatrix(stem_docs)
+dtm_docs
+inspect(dtm_docs[340:345,1:10])
+class(dtm_docs); dim(dtm_docs)
+
+############################
+### Exploration Analysis ###
+############################
+# Exploring the Document Term Matrix
+freq <- colSums(as.matrix(dtm_docs))
+length(freq)
+ord <- order(freq)
+freq[head(ord)] # Least frequent terms
+freq[tail(ord)] # Most frequent terms
+
+# Distribution of Term Frequencies
+head(table(freq), 15)
+tail(table(freq), 15)
+# Plot of Frequencies
+
+# Save as csv
+output <- as.matrix(dtm_docs)
+dim(output)
+write.csv(output, file='en_US_blogs.csv')
+save(output, 'en_US_blogs.RData')
