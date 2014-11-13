343bee61cb703de337f84a47d853366e341409e4
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-13 15:08:11 +1100

    milestone report

--- Task_2_Exploratory_analysis.R
+++ Task_2_Exploratory_analysis.R
@@ -26,7 +26,65 @@ setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\SwiftKey')
 setwd('/Users/ivan/Work_directory/SwiftKey')
 Sys.setenv(JAVA_HOME="C:\\Program Files\\Java\\jre7\\")
 rm(list=ls(all=TRUE));gc(reset=TRUE);par(mfrow=c(1,1))
-require(tm); require(SnowballC); require(data.table); library("RWeka")
+require(tm); require(SnowballC); require(data.table)
+require(ggplot2); require(RWeka); require(qdap);
+require(scales); require(gridExtra); require(wordcloud); require(reshape2)
+source('SwiftKey-Natural-language/Task_1.5_Tokenization_func.R')
+
+######################
+### Basic Analysis ###
+######################
+stem_path <- file.path('.','final/en_US')
+dir(stem_path)
+
+con <- file(paste(stem_path, dir(stem_path)[1], sep='/'),'r')
+en_US_blogs <- readLines(con)
+close(con)
+
+con <- file(paste(stem_path, dir(stem_path)[2], sep='/'),'r')
+en_US_news <- readLines(con)
+close(con)
+
+con <- file(paste(stem_path, dir(stem_path)[3], sep='/'),'r')
+en_US_twitter <- readLines(con)
+close(con)
+
+# Lines
+lengths <- c(length(en_US_blogs),length(en_US_news),length(en_US_twitter))
+lengths <- data.frame(lengths)
+lengths$names <- c("blogs","news","twitter")
+
+# Blogs
+word_count_blogs <- lapply(en_US_blogs, function (z) length(unlist(strsplit(z," "))))
+word_count_blogs <- data.frame(do.call("rbind", word_count_blogs))
+names(word_count_blogs) <- "Word Count"
+# News
+word_count_news <- lapply(en_US_news, function (z) length(unlist(strsplit(z," "))))
+word_count_news <- data.frame(do.call("rbind", word_count_news))
+names(word_count_news) <- "Word Count"
+# Twitter
+word_count_twitter <- lapply(en_US_twitter, function (z) length(unlist(strsplit(z," "))))
+word_count_twitter <- data.frame(do.call("rbind", word_count_twitter))
+names(word_count_twitter) <- "Word Count"
+# Combine the three text sources together
+word_counts <- c(sum(word_count_blogs),sum(word_count_news),sum(word_count_twitter))
+word_counts <- data.frame(word_counts)
+word_counts$names <- c("blogs","news","twitter")
+
+basic_summary <- merge.data.frame(lengths, word_counts, by = 'names')
+melt_df <- melt(basic_summary)
+
+# barplot of total word counts
+ggplot(melt_df,aes(x=names,y=value)) + 
+    geom_bar(stat='identity',fill='cornsilk',color='grey60') + facet_wrap(~variable, scales = "free") + 
+    xlab('Source')+ylab('Word Count/Lines') + 
+    theme(legend.position='none')+
+    geom_text(aes(label=format(word_counts,big.mark=","),size=3),labels=comma,vjust=-0.2) +
+    ggtitle('Total Word Count by Text Source')
+
+#################################
+### Transform into Data Frame ###
+#################################
 
 # ngrams_test <- NGramTokenizer(stem_docs, Weka_control(min = 3, max = 3, delimiters = " \\r\\n\\t.,;:\"()?!"))
 BigramTokenizer <- function(x) 
