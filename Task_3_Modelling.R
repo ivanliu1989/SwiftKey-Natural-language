a0b1c32ccfa2cde790c8126efb38a28ca00d0ce0
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-18 14:35:26 +1100

    splitting

--- Task_3_Modelling.R
+++ Task_3_Modelling.R
@@ -105,22 +105,10 @@ quatrtoken <- NGramTokenizer(stem_df, Weka_control(min=4,max=4))
 ## Chunks Spliting ##
 #####################
 load('data_18_Nov_2014/blog_df.RData')
-c <- 100
-chunks <- list()
-for (i in 1:c){
-    chunks[[i]] <- stem_df[(ceiling(nrow(stem_df)/c)*(i-1)+1):(ceiling(nrow(stem_df)/c)*i),1]
-} 
-ngram_chunks <- list()
-for (j in 1:c){
-    ngram_chunks[[j]] <- NGramTokenizer(chunks[[j]], Weka_control(min=3,max=3))    
-}
-ngram_chunks_all <- c()
-for (z in 1:c){
-    ngram_chunks_all <- c(ngram_chunks_all, ngram_chunks[[z]])
-}
-length(ngram_chunks_all); object.size(ngram_chunks_all); gc()
-ngram_freq_tb <- table(ngram_chunks_all)
-
+source('SwiftKey-Natural-language/Task_4.5_ngram_split_func.R')
+split_num <- 100
+grams <- 4
+ngram_pred <- system.time(ngramify(split_num, stem_df, grams))
 
 
 version3 <- function (){
--- Task_4.5_ngram_split_func.R
+++ Task_4.5_ngram_split_func.R
@@ -1,30 +1,30 @@
 ngramify <- function(split_num=100, ngram_df, grams = 3){
     require(RWeka)
-    cat(paste('Input data frame (rows:',nrow(ngram_df), 'size:',object.size(ngram_df)/1024/1024,
-              'mb \n are goint to split into', split_num, 'and', grams, 'grams prediction table...'))
-    cat(paste('/n Start to create chunks...'))
+    cat(paste('Input data frame (rows:',nrow(ngram_df), ') size:',round(object.size(ngram_df)/1024/1024,0),
+              'mb \n are going to split into', split_num, 'and', grams, 'grams prediction table...'))
+    cat(paste('\n Start to create chunks...'))
     chunks <- list()
     for (i in 1:split_num){
         chunks[[i]] <- ngram_df[(ceiling(nrow(ngram_df)/split_num)*(i-1)+1):(ceiling(nrow(ngram_df)/split_num)*i),1]
     } 
     gc()
-    cat(paste('/n Start to convert chunks into n-grams matrix...'))
+    cat(paste('\n Start to convert chunks into n-grams matrix...'))
     ngram_chunks <- list()
     for (j in 1:split_num){
         ngram_chunks[[j]] <- NGramTokenizer(chunks[[j]], Weka_control(min=grams,max=grams))    
     }
     gc()
-    cat(paste('/n Start to integrate chunks into one matrix...'))
+    cat(paste('\n Start to integrate chunks into one matrix...'))
     ngram_chunks_all <- c()
     for (z in 1:split_num){
         ngram_chunks_all <- c(ngram_chunks_all, ngram_chunks[[z]])
     }
     gc()
-    cat(paste('/n Start to calculate the frequency of each term...'))
+    cat(paste('\n Start to calculate the frequency of each term...'))
     ngram_freq_tb <- sort(table(ngram_chunks_all), decreasing=T)
     gc()
-    cat(paste('/n Finishing the process...'))
-    ngram_pred <- data.frame(terms = names(ngram_freq_tb), freq = ngram_freq_tb)
-    return(ngram_pred)
+    cat(paste('\n Finishing the process...'))
+    ngram_pred <- data.frame(terms = names(ngram_freq_tb), freq = ngram_freq_tb, row.names = NULL)
     
+    return(ngram_pred)
 }
