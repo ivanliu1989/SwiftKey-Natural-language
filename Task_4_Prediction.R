6158f8ddb76e5cae6cab3d80ded4c3b24af62f45
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-22 16:09:59 +1100

    update

--- Task_4_Prediction.R
+++ Task_4_Prediction.R
@@ -102,29 +102,87 @@ load('completed/Trigrams_completed.RData')
 load('completed/Bigrams_completed.RData') 
 load('completed/Unigrams_completed.RData') 
 
-save(Bigrams_all,Quatrgrams_all,Trigram_all,Unigrams_all,file='completed/ngrams_completed.RData')
+save(Bigrams_all,Quatrgrams_all,Trigram_all,Unigrams_all,file='completed/ngrams_completed_matrix.RData')
+load('completed/ngrams_completed_matrix.RData') 
+
 dim(Unigrams_all); dim(Bigrams_all); dim(Trigram_all); dim(Quatrgrams_all)
 head(Unigrams_all);head(Bigrams_all);head(Trigram_all);head(Quatrgrams_all)
 
-Trigram_all<-as.data.frame(Trigram_all)
-for (i in 1:nrow(Trigram_all)){
-    Trigram_all$p[i] <- Trigram_all[i,1]/sum(Trigram_all[which(Trigram_all$term==Trigram_all[i,3]),1])
+Trigram_all<-as.data.table(Trigram_all)
+Trigram_all[,ngram:=strsplit(term, '\\s')]
+Trigram_all[, `:=`(w1=sapply(ngram, function(s) s[1]),
+                      w2=sapply(ngram, function(s) s[2]),
+#                       w3=sapply(ngram, function(s) s[3]),
+#                       w4=sapply(ngram, function(s) s[4]),
+                      term=NULL, ngram=NULL)]
+setnames(Unigrams_all, c('freq', 'pred'))
+setnames(Bigrams_all, c('freq', 'w1', 'pred'))
+setnames(Trigram_all, c('freq', 'pred', 'w1','w2'))
+setnames(Quatrgrams_all, c('freq','w1','w2','w3', 'pred'))
+
+object.size(Unigrams_all)
+Unigrams_all <- Unigrams_all[1:10,]
+save(Bigrams_all,Quatrgrams_all,Trigram_all,Unigrams_all,file='completed/ngrams_model.RData')
+Quatrgrams_all <- as.data.table(Quatrgrams_all)
+
+
+###########################
+### Prediction Modeling ###
+###########################
+setkeyv(Bigrams_all, c('w1','freq','pred'))
+setkeyv(Trigram_all, c('w1','w2','freq','pred'))
+setkeyv(Quatrgrams_all, c('w1','w2','w3', 'freq', 'pred'))
+
+system.time(predict <- Quatrgrams_all[list('how', 'are', 'you')])
+predict
+
+require(stringr)
+predictNgrams <- function(input){
+    ## clean input text
+    # remove numbers, punctuations
+    word <- gsub("[^a-zA-Z\n\']", " ", input)
+    # convert all words to lowercase
+    word <- tolower(word)
+    # remove extra spaces
+    trim <- function(x) return(gsub("^ *|(?<= ) | *$", "", x, perl=T))
+    word<-trim(word)      
+    
+    str <- unlist(str_split(word," "))
+    len <- length(str)
+    
+    predict <- c()
+    
+    if (len>=3){
+        ##trigram 
+        W1 <- str[len-2]; W2 <- str[len-1]; W3 <- str[len]
+        ngram <- Quatrgrams_all[list(W1, W2, W3)]
+        predict <- head(trigram[order(trigram$freq, decreasing=T),]$pred)
+        
+        if(length(predict)<6){
+            ##bigram
+            ngram <- Trigram_all[list(W2, W3)]
+            predict <- c(predict,head(ngram[order(ngram$freq, decreasing=T),]$pred))    
+        }
+        
+        if(length(predict)<6){
+            ##unigram
+            ngram <- Bigrams_all[list(W3)]
+            predict <- c(predict,head(ngram[order(ngram$freq, decreasing=T),]$pred))    
+        }
+        
+        predict <- predict[!is.na(predict)]
+        
+        if(length(predict)<5){
+            predict <- c(predict, Unigrams_all$pred[1:5])
+        }
+    }    
+    return(predict)
 }
 
+input <- 'tell me something about'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+system.time(result <- predictNgrams(input))
+result
 
 
 
